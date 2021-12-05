2b567cc974f81fe4963af71d1b92dd07597df8f3
btijerino16 <92263027+btijerino16@users.noreply.github.com>
2021-12-05 13:53:09 -0700

    added current date and time to app

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var currentHour = moment().hour();
+
+// current date displays when page opens up 
+
+$("#currentDay").text(moment().format('dddd, MMMM Do YYYY, h:mm a'));
+
+// function that adds color to blocks 
+
+
+//function that loads times
+
+
+// click alerts and functionality 
+
