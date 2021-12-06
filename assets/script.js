dd83d452d7dc38c042ca2cea89e1b3153894e973
btijerino16 <92263027+btijerino16@users.noreply.github.com>
2021-12-05 20:29:10 -0700

    addition psuedo ideas'

--- assets/script.js
+++ assets/script.js
@@ -6,18 +6,33 @@ $("#currentDay").text(moment().format('dddd, MMMM Do YYYY, h:mm a'));
 
 // function that adds color to blocks 
 
+function hoverColor ()
+
+
 
 //function that loads times
+ 
+function loadTimes () {
 
+
+    
+
+
+
+ }
+
+ // function to show save task when refreshed
+
+ 
 
 // click alerts and functionality 
 
 $(".saveBtn").click(function () {
     event.preventDefault();
-    var formValue = $(this).siblings(".form-control").val();
+    var columnControl = $(this).siblings(".col-10-control").val();
     console.log("BETTER WORK");
     var listItem = $(this).parent().data("hour");
 
-    localStorage.setItem(listItem, formValue);
+    localStorage.setItem(listItem, columnControl);
 });
 
